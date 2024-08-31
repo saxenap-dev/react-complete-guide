7a80c200f75a510a70fd787b89c07c2ece61cdb5
Saxena, Pulkit <SaxenaP@aetna.com>
2024-08-31 16:35:20 -0500

    reacting to click event

---
+++ 03 React Essentials/10 reacting to events/src/components/TabsButton/TabButton.jsx
@@ -0,0 +1,8 @@
+export default function TabButton({children}) {
+    function handleClick() {
+        console.log('Button Clicked');
+    }
+    return (
+        <li><button onClick={handleClick}>{children}</button></li>
+    );
+}
