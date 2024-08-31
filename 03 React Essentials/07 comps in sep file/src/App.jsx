9e1f1571b29135ed8139bb06f6980e6f0f5a7f5f
Saxena, Pulkit <SaxenaP@aetna.com>
2024-08-31 11:51:27 -0500

    storing files in sep file

---
+++ 03 React Essentials/07 comps in sep file/src/App.jsx
@@ -0,0 +1,27 @@
+
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header';
+import CoreConcepts from './components/CoreConcepts';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main>
+        <div id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+            {/* passing props by destructuring */}
+            <CoreConcepts { ...CORE_CONCEPTS[1]}/>
+            <CoreConcepts { ...CORE_CONCEPTS[2]}/>
+            <CoreConcepts { ...CORE_CONCEPTS[3]}/>
+          </ul>
+        </div>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ 03 React Essentials/07 comps in sep file/src/components/CoreConcepts.jsx
@@ -0,0 +1,9 @@
+export default function CoreConcepts({title, image, description}) { // destructured the props
+    return (
+      <li>
+        <img src={image} alt={title}/>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    );
+  }
