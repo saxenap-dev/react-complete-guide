0a42423b8d6e35707b6d49caf1df6e8c51cd64e4
Saxena, Pulkit <SaxenaP@aetna.com>
2024-09-01 00:54:15 -0500

    deriving computing values

---
+++ 03 React Essentials/15 deriving computing values/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header';
+import CoreConcepts from './components/CoreConcepts/CoreConcepts';
+import TabButton from './components/TabsButton/TabButton';
+import { EXAMPLES } from './data';
+
+function App() {
+  const [tabContent, setTabContent] = useState('components');
+
+  function onSelect(selectedButton) {
+    setTabContent(selectedButton);
+  }
+  return (
+    <div>
+      <Header/>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+            {/* passing props by destructuring */}
+            <CoreConcepts { ...CORE_CONCEPTS[1]}/>
+            <CoreConcepts { ...CORE_CONCEPTS[2]}/>
+            <CoreConcepts { ...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+        <section id="examples">
+          <menu>
+            <TabButton onSelect={() => onSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => onSelect('jsx')}>JSX UI</TabButton>
+            <TabButton onSelect={() => onSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => onSelect('state')}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[tabContent].title}</h3>
+            <p>{EXAMPLES[tabContent].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[tabContent].code}
+              </code>
+            </pre>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
