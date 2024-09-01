744af9221addf355281a2cd1f9b4cd901550d00b
Saxena, Pulkit <SaxenaP@aetna.com>
2024-09-01 01:18:54 -0500

    outputting list data

---
+++ 03 React Essentials/18 outputting list data/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header';
+import CoreConcepts from './components/CoreConcepts/CoreConcepts';
+import TabButton from './components/TabsButton/TabButton';
+import { EXAMPLES } from './data';
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState('');
+
+  function onSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a button!</p>
+
+  if (selectedTopic) {
+    tabContent = <div>
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>
+          {EXAMPLES[selectedTopic].code}
+        </code>
+      </pre>
+    </div>
+  }
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcepts key={conceptItem.title} {...conceptItem} />)}
+            
+          </ul>
+        </section>
+        <section id="examples">
+          <menu>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => onSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => onSelect('jsx')}>JSX UI</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => onSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => onSelect('state')}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            {/* {!tabContent && <p>Please select a button</p>}
+            {tabContent && (
+              <div>
+                <h3>{EXAMPLES[tabContent].title}</h3>
+                <p>{EXAMPLES[tabContent].description}</p>
+                <pre>
+                  <code>
+                    {EXAMPLES[tabContent].code}
+                  </code>
+                </pre>
+              </div>
+            )} */}
+            {tabContent}
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
